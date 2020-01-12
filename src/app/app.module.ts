77e61ffa4b8327119f6f9f3326f7e48ba46042d3
Igor Gavrilyuk <igorek1210@gmail.com>
2020-01-13 02:31:45 +0300

    Create summary component with static data

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SummaryComponent } from './components/summary/summary.component';
+import { TypeSummaryComponent } from './components/type-summary/type-summary.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SummaryComponent,
+    TypeSummaryComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/type-summary/type-summary.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-type-summary',
+  templateUrl: './type-summary.component.html',
+  styleUrls: ['./type-summary.component.css']
+})
+export class TypeSummaryComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
